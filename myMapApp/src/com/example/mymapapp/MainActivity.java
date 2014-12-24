4ed5798b7bdc3a5946b82da98a367ca818aea96d
Rahool <rahool@saedutech.com>
2014-12-25 01:04:05 +0530

    marker added , set initial map position and zoom level

--- myMapApp/src/com/example/mymapapp/MainActivity.java
+++ myMapApp/src/com/example/mymapapp/MainActivity.java
@@ -2,38 +2,42 @@ package com.example.mymapapp;
 
 import android.app.Activity;
 import android.app.Dialog;
+import android.app.SearchManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.SearchView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 
 public class MainActivity extends Activity {
 	
 	private static final int GPS_ERRORDIALOG_REQUEST = 9001;
 	GoogleMap mMap;
-	
-	
+	static final LatLng jalgaonLatLng = new LatLng(20.9980, 75.5667);
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
-        
+
         if (servicesOK()) {
         	setContentView(R.layout.map_activity);
         	Toast.makeText(this, "Google service are available", Toast.LENGTH_SHORT).show();
         	mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
         	mMap.setMyLocationEnabled(true);
+        	Marker jal = mMap.addMarker(new MarkerOptions().position(jalgaonLatLng).title("Jalgaon City"));
         }else{
         	setContentView(R.layout.activity_main);
         }
-        
     }
 
     public boolean servicesOK()
@@ -55,6 +59,15 @@ public class MainActivity extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+        
+        // Associate searchable configuration with the SearchView
+        SearchManager searchManager =
+               (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView =
+                (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setSearchableInfo(
+                searchManager.getSearchableInfo(getComponentName()));
+
         return true;
     }
     
@@ -70,22 +83,16 @@ public class MainActivity extends Activity {
 		case R.id.mapTypeSatellite:
 			mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
 			break;
-			
 		case R.id.mapTypeTerrain:
 			mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
 			break;
 		case R.id.mapTypeHybrid:
 			mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
 			break;
-		
-
 		default:
 			break;
 		}
-
 		return super.onOptionsItemSelected(item);
 	}
-
-   
-    
+ 
 }
